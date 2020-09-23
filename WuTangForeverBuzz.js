925b1f1a7017bc588587254bf025d5ea90527bd0
marsha <marshaannon@gmail.com>
2020-09-23 18:41:43 -0400

    complete exercise

--- WuTangForeverBuzz.js
+++ WuTangForeverBuzz.js
@@ -1,9 +1,20 @@
 class WuTangForeverBuzz {
     wuTangClan(){
-        let output ="";
-
+        let output = "";
+        for (let i = 1; i <= 15; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                output += "WuTangForever\n"
+            } else if (i % 3 === 0) {
+                output += "Wu\n"
+            } else if (i % 5 === 0) {
+                output += "Tang\n"
+            } else {
+                output += i + "\n"
+            }
+        }
         return output;
     }
 }
+
 
 module.exports = WuTangForeverBuzz;
